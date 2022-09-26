25c67c0b749d5c7f106a98df4e2c3936ce013e64
Helin Çelikbaş <helincelikbas142@gmail.com>
2022-09-26 12:13:40 +0300

    SeyahatRezervasyonSitesi
    Seyahat Rezervasyon Sitesi

---
+++ GeziSitesi_CoreProje/GeziSitesi_CoreProje/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GeziSitesi_CoreProje.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
